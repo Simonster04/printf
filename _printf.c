0b4984bb96742024734a2b28ec6c0c06e3fadb2e
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-28 01:27:57 +0000

    test

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: char with the specific format to be printed
+ *
+ * Return: 0 on success.
+ */
+
+int _printf(const char *format, ...)
+{
+
+	int i = 0, cont, z;
+	char arg, *s;
+
+	va_list valist;
+
+	va_start(valist, format);
+
+	for (cont = 0; format[cont] != '\0'; cont++)
+	{
+	}
+	while (i < cont)
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+			case 'c':
+			arg = va_arg(valist, int);
+			write(1, &arg, 1);
+			i++;
+			break;
+			case 's':
+			s = va_arg(valist, char*);
+			for (z = 0; s[z] != '\0'; z++)
+				write(1, &s[z], 1);
+			i++;
+			break;
+			default:
+			break;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+		}
+	i++;
+	}
+	va_end(valist);
+	return (cont);
+}
