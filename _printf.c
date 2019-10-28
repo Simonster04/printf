2693f8bdd3d7f40e17d4652c595bf13b6704cecd
Simon Parra <simon.pahe@gmail.com>
2019-10-28 15:22:44 +0000

    fixing tabulation in _printf.c

--- _printf.c
+++ _printf.c
@@ -15,46 +15,44 @@
 int _printf(const char *format, ...)
 {
 
-        int i = 0, cont, z;
-        char arg, *s;
+	int i = 0, cont, z;
+	char arg, *s;
 
-        va_list valist;
+	va_list valist;
 
-        va_start(valist, format);
+	va_start(valist, format);
 
-        for (cont = 0; format[cont] != '\0'; cont++)
-        {
-        }
-
-        while (i < cont && cont != 0)
-        {
-                if (format[i] == '%' && (format[i + 1] == 's' || format[i + 1] == 'c'))
-                {
-                        switch (format[i + 1])
-                        {
-                        case 'c':
-                        arg = va_arg(valist, int);
-                        write(1, &arg, 1);
-                        i++;
-                        break;
-                        case 's':
-                        s = va_arg(valist, char*);
-                        for (z = 0; s[z] != '\0'; z++)
-                                write(1, &s[z], 1);
-                        i++;
-                        break;
-                        default:
-                        break;
-                        }
-                }
-                else
-                {
-                        write(1, &format[i], 1);
-                }
-        i++;
-
-        }
-        va_end(valist);
-        return (cont);
+	for (cont = 0; format[cont] != '\0'; cont++)
+	{
+	}
+	while (i < cont && cont != 0)
+	{
+		if (format[i] == '%' && (format[i + 1] == 's' || format[i + 1] == 'c'))
+		{
+			switch (format[i + 1])
+			{
+			case 'c':
+	arg = va_arg(valist, int);
+			write(1, &arg, 1);
+			i++;
+			break;
+			case 's':
+			s = va_arg(valist, char*);
+			for (z = 0; s[z] != '\0'; z++)
+				write(1, &s[z], 1);
+			i++;
+			break;
+			default:
+			break;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+		}
+		i++;
+	}
+	va_end(valist);
+	return (cont);
 }
 
