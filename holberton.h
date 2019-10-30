ac9308a03d28fa0a02476cba76b0d38afb982e3c
Simon Parra <simon.pahe@gmail.com>
2019-10-30 20:20:47 +0000

    Darwin the crack

--- holberton.h
+++ holberton.h
@@ -1,12 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdarg.h>
-
 int print_number(int n);
 int _printf(const char *format, ...);
 int _putchar(char c);
-int flags(char c, va_list *valist);
 
 #endif
 
