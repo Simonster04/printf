280121554d47637bd7394b62354fd4eec9dec5e0
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-30 03:32:38 +0000

    add the file .h

--- holberton.h
+++ holberton.h
@@ -1,9 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdarg.h>
+
 int print_number(int n);
 int _printf(const char *format, ...);
 int _putchar(char c);
+int flags(char c, va_list *valist);
 
 #endif
 
