a70c75ee8025e5676b9643e1b2a05b61a15428fc
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-10-30 04:26:47 +0000

    Again

--- _printf.c
+++ _printf.c
@@ -11,11 +11,12 @@
 int _printf(const char *format, ...)
 {
 	va_list valist;
-	int i, bytes = 0;
+	char arg = '\0';
+	int i, x, num = 0, bytes = 0;
+	unsigned char  *s = 0;
 
 	if (!format)
 	{return (-1); }
-
 	va_start(valist, format);
 
 	for (i = 0; format[i]; i++)
@@ -26,8 +27,42 @@ int _printf(const char *format, ...)
 		{
 			while (format[i + 1] == ' ')
 			{i++; }
-			bytes += flags(format[i + 1], &valist);
+			switch (format[i + 1])
+			{
+			case 'c':
+			arg = va_arg(valist, int);
+			_putchar(arg);
 			i++;
+			break;
+			case 's':
+			s = va_arg(valist, unsigned char*);
+				if (s)
+				{
+				for (x = 0; s[x]; x++)
+				{	_putchar(s[x]);
+					bytes++;
+				}
+				}
+			bytes--;
+			i++;
+			break;
+			case 'd': case 'i':
+			num = va_arg(valist, int);
+			bytes += print_number(num);
+			bytes--;
+			i++;
+			break;
+			case '%':
+			_putchar('%');
+			i++;
+			break;
+			case '\0':
+			bytes = -2;
+			break;
+			default:
+			_putchar('%');
+			break;
+			}
 		}
 		else
 		{
